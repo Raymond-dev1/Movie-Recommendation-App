b362566956dd56d8de821a671fcb1bb7e3be2c33
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-02-07 23:31:11 +0100

    add useeffect to movie display

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,7 @@ export default function MovieCard ({movie}){
 
     return<div className="p-8 m-1 shadow-xl/20 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
         <div className = "">
-            <img className="rounded-xl" src ={movie.url} alt={movie.title} /> 
+            <img className="rounded-xl" src ={`https://image.tmdb.org/t/p/w500${movie.poster_path   }`} alt={movie.title} /> 
             <div className="overlay">
                 <button className="favorite" onClick={addToFavorite}>
                     {like}
@@ -19,7 +19,7 @@ export default function MovieCard ({movie}){
             </div>
             <div className=" movie-title  ">
                <span className="title  text-2xl" ><h3> {movie.title}</h3></span>
-               <p>{movie.release_date}</p>
+               <p>{movie.release_date.split('-')[0]}</p>
             </div>
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,31 @@
+import { useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
+import { getPopularMovies, searchMovies } from "../services/api";
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
-  const movies = [
-    {
-      id: 0,
-      title: "Big One",
-      release_date: "2021",
-      url: "https://images.unsplash.com/photo-1768938896473-fa08fb0f20ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfGhtZW52UWhVbXhNfHxlbnwwfHx8fHw%3D",
-    },
-    {
-      id: 1,
-      title: "Big Old One",
-      release_date: "2022",
-      url: "https://images.unsplash.com/photo-1764069138714-bcc6783b8e48?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
-    },
-    {
-      id: 2,
-      title: "Geek One",
-      release_date: "2025",
-      url: "https://images.unsplash.com/photo-1765677707084-d2ac28ad4bf6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 3,
-      title: "Real  one",
-      release_date: "2022",
-      url: "https://images.unsplash.com/photo-1769107418962-ec5ecd084014?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNXx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
+  const [movies , setMovies ] = useState([])
+  const [error, setError] =useState(null)
+
+
+useEffect( () => {
+
+  const loadPopularMovies = async () =>{
+    try{
+      const popularMovies = await getPopularMovies()
+      setMovies(popularMovies)
+    }catch(err){
+      console.log (err)
+      setError('Error loading movies')
+    }finally{
+      setLoading(false)
+        }
+  }
+  loadPopularMovies()
+
+}, [])
 
 
   const handleSearch = (e) => {
@@ -62,14 +58,21 @@ export default function Home() {
           disabled={loading}
         >
           {loading? "Searching..." : "Search"}
-          {/* <span className=" text-center "></span>Search */}
+         
         </button>
       </form>
+
+      {error &&<div className="text-red-500 ">{error}</div>}
+
+      {loading ? ( 
+        <div className="">Loading...</div>
+      ) : (
       <div className="movie-card inline-grid grid-cols-3 gap-4 shadow-xl ">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
+      )}
     </div>
   );
 }
