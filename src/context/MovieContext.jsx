d9122a2038653d1bbdf43d68d0fbf6a00c01e54e
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-02-08 09:38:41 +0100

    add movie context

---
+++ src/context/MovieContext.jsx
@@ -0,0 +1,41 @@
+import { useContext, useState, createContext, useEffect  } from "react";
+
+
+const MovieContext =  createContext()
+
+export const useMovieContext = () => useContext(MovieContext)
+
+export const MovieProvider = ({children}) => {
+
+    const[ favorites , setFavorites ] = useState
+
+    useEffect ( () => {
+        const storedFavs = localStorage.setItem("favorites")
+
+        if(storedFavs) setFavorites(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect (()=>{
+        localStorage.getItem( "favorites" , JSON.stringify(favorites))
+    } ,[favorites])
+
+     const addToFavorite = (movie) => {
+        setFavorites(prev =>[...prev , movie])
+    }
+    const removeFavorite =(movieId) =>{
+         setFavorites(prev=>prev.filter(movie =>movie.id !== movieId))
+    }
+    const isFavorite = (movieId) =>{
+        return favorites.some(movie => movie.id===movieId)
+    }
+
+    const value = {
+        addToFavorite,
+        removeFavorite,
+        isFavorite
+    }
+
+    return <MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+}
